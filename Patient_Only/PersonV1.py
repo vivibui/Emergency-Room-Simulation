a25b2a6bffb6317b4a6269aa43f0d011901144a0
vivibui <vivianbui9889@gmail.com>
2023-02-07 11:59:14 -0600

    Add code attribute to Person class - Patient Only

--- Patient_Only/PersonV1.py
+++ Patient_Only/PersonV1.py
@@ -8,17 +8,22 @@
 #############################################
 
 # ID is an integer 
+# Code is a string 
 
 class Person:
     def __init__(self, **kwargs): 
         self.__id = kwargs['id'] 
-        
+        self.__code = kwargs['code']
+                
 
     ################################################   
     ###################  Get Method ################
 
     def get_id(self): 
         return self.__id 
+
+    def get_code(self):
+        return self.__code 
 
 
     ################################################   
