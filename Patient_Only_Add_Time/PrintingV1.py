3b54a2ec625d9e1f20e94c5bb6746fdb3a654446
vivibui <vivianbui9889@gmail.com>
2023-02-08 22:11:22 -0600

    Edit export

--- Patient_Only_Add_Time/PrintingV1.py
+++ Patient_Only_Add_Time/PrintingV1.py
@@ -14,7 +14,7 @@ def LogHeader(day):
     print(format(f'DAY {day}', "10s"))
     print('----------------------------------------')
     header = format(f'Day_{day}', '10s') + format("New_Patients", "20s") + format("Waiting", "15s") \
-        + format("Admitted", "10s") + format("Released", "10s") + format("Beds_Avail_EOH", "15s")
+        + format("Admitted", "10s") + format("Released", "10s") + format("Beds_Avail_EOH", "15s") + "\n"
     print(header) 
     return header
     
@@ -36,7 +36,7 @@ def ListPatients(emergency_room):
     header_patients =  format("ID", "10s") + format("Day_C", "10s") + format("Time_C", "20s") + \
                 format("Age", "10s") + format("AcuteLv", "10s") + format("PainLv", "10s") \
                 + format("Status", "10s") + format("Day_A", "10s") + format("Time_A", "20s") \
-                    + format("Day_R", "10s") + format("Time_R", "20s") + format("Time_in_ER", "25s") 
+                    + format("Day_R", "10s") + format("Time_R", "20s") + format("Time_in_ER", "25s") + "\n"
     print(header_patients)
     print()
     for patient in emergency_room.get_patients(): 
