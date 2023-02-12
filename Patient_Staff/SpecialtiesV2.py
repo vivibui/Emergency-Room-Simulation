3b80ee29fa7053257ea768ce72859d965eb0f051
vivibui <vivianbui9889@gmail.com>
2023-02-11 21:40:56 -0600

    wip

--- Patient_Staff/SpecialtiesV2.py
+++ Patient_Staff/SpecialtiesV2.py
@@ -1,47 +1,12 @@
 
 
 
-# From 0 - 20 
 def SpecialtiesList(s): 
-    if s == 1: 
-        print('Injury and poisoing')
-    elif s == 2: 
-        print('Symptoms, signs and abnormal findings') 
-    elif s == 3: 
-        print('Respiratory system') 
-    elif s == 4: 
-        print('Musculoskeletal system and connective tissue') 
-    elif s == 5: 
-        print('Circulatory system') 
-    elif s == 6: 
-        print('Digestive system') 
-    elif s == 7: 
-        print('Genitourinary system') 
-    elif s == 8: 
-        print('Nervous system') 
-    elif s == 9: 
-        print('Mental, behavioral and neurodevelopmental')
-    elif s == 10: 
-        print('Skin and subcutaneous') 
-    elif s == 11:
-        print('Pregnancy, childbirth and the puerperium') 
-    elif s == 12: 
-        print('Ear and mastoid process') 
-    elif s == 13: 
-        print('Factors influencing health status')
-    elif s == 14: 
-        print('Infectious and parasitic') 
-    elif s == 15: 
-        print('Endocrine, nutritional and metabolic') 
-    elif s == 16: 
-        print('Eye and adnexa') 
-    elif s == 17: 
-        print('Blood and blood forming organs')
-    elif s == 18: 
-        print('Neoplasms') 
-    elif s == 19: 
-        print('Conditions originating in the perinatal period') 
-    elif s == 20: 
-        print('Congenital malformations and abnormalities') 
-    else: # s == 0 
-        print('Other') 
+    pass 
+
+def isSurgery(s): 
+    pass 
+
+
+def isMental(s): 
+    pass 
