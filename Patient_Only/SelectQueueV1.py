a69f4ed38752ae6b31d9c710bea0c112b0dd7bef
vivibui <vivianbui9889@gmail.com>
2023-04-06 21:28:28 -0500

    simplify code
    Simplify logic for C1 and C2

--- Patient_Only/SelectQueueV1.py
+++ Patient_Only/SelectQueueV1.py
@@ -1,6 +1,7 @@
 
 import ConfigV1 as cf 
 
+FLAG = 1 
 
 '''
 #################################################################  
@@ -9,62 +10,118 @@ import ConfigV1 as cf
 def SelectFromQueue(queue, day, time):
     return queue.get_queue()[0]
 
-'''
+
 #################################################################  
-# METHOD 2: C1 - Rule stated below
+# METHOD 2: C1 
 ################################################################# 
 
-def SelectFromQueue(queue, day, time):
-    # Initialize
-    max_wait = 0 
-    min_score = 10000 
-    # Get patient to assign to bed 
+# Get patient to assign to bed 
         #### Before Benchmark W: Priority Score > FIFO 
-        #### After Benchmark W: LIFO 
+        #### After Benchmark W: FIFO 
         # First, a person who has been waiting the longest 
             # and their wait time is more than BENCHMARK_W hour will be prioritized 
         # Second, base on priority score: the lower the higher the priority 
         # If multiple patients have the same min priority score, then base on order: 
             # the smaller the id the higher the priority 
-    for patient in queue.get_queue(): 
-        person_wait_time = patient.calc_wait_time(day, time) 
-        person_priority_score = patient.get_priority_score() 
-        if person_wait_time > max_wait: 
-            max_wait = person_wait_time 
-            max_patient = patient 
-        if person_priority_score < min_score: 
-            min_score = person_priority_score
-            min_patient = patient 
-    if max_wait >= cf.BENCHMARK_W: 
-        return max_patient 
-    else: 
-        return min_patient 
-    
-'''
-#################################################################  
-# METHOD 3: C2 - Rule stated below
-################################################################# 
 
 def SelectFromQueue(queue, day, time):
     # Initialize
-    max_wait = 0 
     min_score = 10000 
-    # Get patient to assign to bed 
+
+    all_waiting = queue.get_queue()
+
+    if len(all_waiting) == 1: 
+        return all_waiting[0]
+    
+    for patient in all_waiting: 
+        person_priority_score = patient.get_priority_score() 
+        if person_priority_score < min_score: 
+            min_score = person_priority_score
+            min_patient = patient 
+    if all_waiting[0].calc_wait_time(day, time) >= cf.BENCHMARK_W: 
+        return all_waiting[0]
+    else: 
+        return min_patient 
+'''    
+
+#################################################################  
+# METHOD 3: C2 
+################################################################# 
+
+# Get patient to assign to bed 
         #### Before Benchmark W: Priority Score > FIFO 
         #### After Benchmark W: LIFO 
         # First, base on priority score: the lower the higher the priority 
         # If the same min priority score, then base on order: the smaller the id the higher the priority 
         # If maximum wait time of a patient reached Benchmark W -> LIFO 
-    for patient in queue.get_queue(): 
-        person_wait_time = patient.calc_wait_time(day, time) 
+
+def SelectFromQueue(queue, day, time):
+    # Initialize
+    min_score = 10000 
+    
+    all_waiting = queue.get_queue()
+
+    if len(all_waiting) == 1: 
+        return all_waiting[0]
+    
+    for patient in all_waiting: 
         person_priority_score = patient.get_priority_score() 
-        if person_wait_time > max_wait: 
-            max_wait = person_wait_time 
         if person_priority_score < min_score: 
             min_score = person_priority_score
             min_patient = patient 
-    if max_wait >= cf.BENCHMARK_W: 
-        return patient # LIFO applied after when a queuing patient with max wait time reached benchmark W 
+    if all_waiting[0].calc_wait_time(day, time) >= cf.BENCHMARK_W: 
+        return all_waiting[-1] # LIFO apply when the patient with the longest wait time hits Benchmark W 
     else: 
-        return min_patient
+        return min_patient 
+
+
 '''
+#################################################################  
+# METHOD 4: A1 - Alternate 
+################################################################# 
+
+# Get patient to assign to bed 
+        #### Before Benchmark W: Based on Priority Score 
+        #### After Benchmark W: Alternate 
+        # First, base on priority score: the lower the higher the priority 
+        # If multiple patients have the same min priority score, then base on order: 
+            # the smaller the id the higher the priority 
+        # If the wait time of first patient in queue passes Bechnmark W: 
+            # Alternate between selecting the max and min priority score 
+            # If multiple patients have the same min and max score, thenn base on the order
+
+'''   
+
+
+#################################################################  
+# METHOD 5: A2 - Alternate with Two Probes 
+################################################################# 
+
+# Get patient to assign to bed 
+        #### Before Benchmark W: Based on Priority Score 
+        #### After Benchmark W: Alternate 
+        # First, base on priority score: the lower the higher the priority 
+        # If multiple patients have the same min priority score, then base on order: 
+            # the smaller the id the higher the priority 
+        # If the wait time of first patient in queue passes Bechnmark W: 
+            # Divide the queue into half 
+            # Alternate between selecting the max and min priority score in proble 1 and 2 
+            # If multiple patients have the same min and max score, then base on the order
+
+
+
+#################################################################  
+# METHOD 6: A3 - Alternate with Multiple Probes 
+################################################################# 
+
+# Get patient to assign to bed 
+        #### Before Benchmark W: Based on Priority Score 
+        #### After Benchmark W: Alternate 
+        # First, base on priority score: the lower the higher the priority 
+        # If multiple patients have the same min priority score, then base on order: 
+            # the smaller the id the higher the priority 
+        # If the wait time of first patient in queue passes Bechnmark W: 
+            # Divide the queue into half 
+            # Alternate between selecting the max and min priority score as in probe order 
+                # (User specify the order of probes)
+            # If multiple patients have the same min and max score, then base on the order
