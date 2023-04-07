98d364f8349f8b25c622c43cfdeaccff308dfc5a
vivibui <vivianbui9889@gmail.com>
2023-04-07 00:58:55 -0500

    push_to_end in queue
    Add push_to_end()

--- Patient_Only/QueueV1.py
+++ Patient_Only/QueueV1.py
@@ -8,7 +8,15 @@ class Queue:
         self.__queue.append(patient)
 
     def dequeue(self, patient): 
-        self.__queue.remove(patient)
+        if self.__queue: 
+            self.__queue.remove(patient)
+
+    def push_to_end(self): 
+        # Push a patient from the first in queue to the end of queue
+        if self.__queue: 
+            patient = self.__queue.pop(0)
+            self.__queue.append(patient)
+            return patient
 
     def get_queue(self): 
         return self.__queue
