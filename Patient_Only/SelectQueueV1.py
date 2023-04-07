3fb71d11ba05e4717d29a326bed963a7f9260fd4
vivibui <vivianbui9889@gmail.com>
2023-04-06 21:37:33 -0500

    add method A1
    New method in SelectQueue

--- Patient_Only/SelectQueueV1.py
+++ Patient_Only/SelectQueueV1.py
@@ -42,7 +42,7 @@ def SelectFromQueue(queue, day, time):
         return all_waiting[0]
     else: 
         return min_patient 
-'''    
+   
 
 #################################################################  
 # METHOD 3: C2 
@@ -74,8 +74,8 @@ def SelectFromQueue(queue, day, time):
     else: 
         return min_patient 
 
+''' 
 
-'''
 #################################################################  
 # METHOD 4: A1 - Alternate 
 ################################################################# 
@@ -89,8 +89,41 @@ def SelectFromQueue(queue, day, time):
         # If the wait time of first patient in queue passes Bechnmark W: 
             # Alternate between selecting the max and min priority score 
             # If multiple patients have the same min and max score, thenn base on the order
+def SelectFromQueue(queue, day, time):
+    # Initialize
+    min_score = 10000 
+    
+    all_waiting = queue.get_queue()
 
-'''   
+    if len(all_waiting) == 1: 
+        return all_waiting[0]
+
+    for patient in all_waiting: 
+        person_priority_score = patient.get_priority_score() 
+        if person_priority_score < min_score: 
+            min_score = person_priority_score
+            min_patient = patient 
+    
+    if all_waiting[0].calc_wait_time(day, time) >= cf.BENCHMARK_W:
+        if len(all_waiting)%2 == 0: 
+            x = len(all_waiting)//2
+        else: 
+            # If the number of patients in queue is odd 
+                # probe 2 will larger than probe 1 by one patient
+                # since probe 2 stores the last patient 
+            x = (len(all_waiting)-1)//2
+        probe1 = all_waiting[:x]
+        probe2 = all_waiting[x:] 
+        global FLAG
+        if FLAG == 1: 
+            FLAG = 2
+            return probe2[0]
+        else: 
+            FLAG = 1
+            return probe1[0]
+    else: 
+        return min_patient 
+
 
 
 #################################################################  
